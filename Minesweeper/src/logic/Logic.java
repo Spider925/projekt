20af4c46bfda8c67df6c4d729c758275e6b6d8a3
sanapci <sanapci@caesar.elte.hu>
2017-05-20 21:07:38 +0200

    Logic kezdetek2

--- Minesweeper/src/logic/Logic.java
+++ Minesweeper/src/logic/Logic.java
@@ -11,7 +11,20 @@ public class Logic {
         table = new Table();
     }
     
-    public String getMinesCountAround(int a,int b){
+    public void startGame(int a,int b, int c){
+        table.newGame(a,b,c);
+    }
+    
+    public String clickAction(int a,int b){
+        String result;
+        
+        if( table.getField(a,b).isMine() ) result = "-1";
+        else result = Integer.toString(getMinesCountAround(a,b));
+        
+        return result;
+    }
+    
+    private int getMinesCountAround(int a,int b){
         int c = 0;
         
         for(int i=a-1;i<=a+1;i++) for(int j=b-1;j<=+1;j++){
@@ -20,6 +33,6 @@ public class Logic {
             if( table.getField(i,j).isMine() ) c++;
         }
         
-        return Integer.toString(c);
+        return c;
     }
 }
