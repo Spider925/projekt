9bbe8a829d60d252f5384c215b6b70a73dfa6c97
sanapci <sanapci@caesar.elte.hu>
2017-05-20 20:48:14 +0200

    Logic kezdetek2

---
+++ Minesweeper/src/logic/Logic.java
@@ -0,0 +1,25 @@
+package logic;
+
+/**
+ *
+ * @author sanapci
+ */
+public class Logic {
+    private final Table table;
+    
+    public Logic(){
+        table = new Table();
+    }
+    
+    public String getMinesCountAround(int a,int b){
+        int c;
+        
+        for(int i=a-1;i<=a+1;i++) for(int j=b-1;j<=+1;j++){
+            if( i == a && j == b ) continue;
+            
+            
+        }
+        
+        return Integer.
+    }
+}
