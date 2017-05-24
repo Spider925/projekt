58e65bbd4b3f8e505494158e924e5d3aeca8cb50
Spider <Spider@Spider-PC>
2017-05-24 21:41:59 +0200

    gui csiszolgatás

--- Minesweeper/src/minesweeper/view/SettingsWindow.java
+++ Minesweeper/src/minesweeper/view/SettingsWindow.java
@@ -17,29 +17,43 @@ public class SettingsWindow extends JFrame
         
         // Az ablak beallitasai
         setTitle("Aknakereső - Új játék");
-        setSize(400,400);
+        setSize(50,180);
         setLocationRelativeTo(null);
         setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
                 
         // Panel és megjelenítés
         setLayout(new BorderLayout());
         
+        JLabel info = new JLabel("Válassz nehézséget");
+        info.setHorizontalAlignment(JLabel.CENTER);
+        add(info, BorderLayout.PAGE_START);
+        
+        JPanel diff = new JPanel();
+        
         JRadioButton easybutton = new JRadioButton("Könnyű");
         JRadioButton normalbutton = new JRadioButton("Közepes");
         JRadioButton hardbutton = new JRadioButton("Nehéz");
+        
+        easybutton.setActionCommand("easy");
+        normalbutton.setActionCommand("normal");
+        hardbutton.setActionCommand("hard");
         
         difficulty = new ButtonGroup();
         difficulty.add(easybutton);
         difficulty.add(normalbutton);
         difficulty.add(hardbutton);
         
+        diff.add(easybutton);
+        diff.add(normalbutton);
+        diff.add(hardbutton);
         
-        add(easybutton, BorderLayout.NORTH);
+        add(diff);
         
         startgame = new JButton("OK");
         startgame.addActionListener(this);
-        add(startgame, BorderLayout.SOUTH);        
+        add(startgame, BorderLayout.SOUTH);       
+
         
         setVisible(true);
     }
@@ -47,6 +61,20 @@ public class SettingsWindow extends JFrame
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == startgame) {
+            try {
+            String selection = difficulty.getSelection().getActionCommand();
+            switch (selection) {
+                case "easy":
+                    break;
+                case "normal":
+                    break;
+                case "hard":
+                    break;
+            }
+            }
+            catch (NullPointerException ex) {
+                JOptionPane.showMessageDialog(null, "Nincs kiválasztva nehézség!");
+            }
             
         }
     }
